63ca181ebbcbac91b224993abfed3241aea0e98e
Nikki <nikkirosecarlos@gmail.com>
2024-05-20 17:50:26 +0900

    add custom user model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = [
+        "email",
+        "username",
+        "preferred_contact",
+        "is_staff",
+    ]
+    fieldsets = UserAdmin.fieldsets + ((None, {"fields": ("preferred_contact",)}),)
+    add_fieldsets = UserAdmin.add_fieldsets + ((None, {"fields": ("preferred_contact",)}),)
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    PREFERRED_CONTACT_CHOICES = [
+    ('facebook', 'Facebook Messenger'),
+    ('line', 'Line'),
+    ('email', 'Email'),
+]
+    
+    preferred_contact = models.CharField(max_length=10, choices=PREFERRED_CONTACT_CHOICES, default='email')
