0e48d8fb9be4dd53fc709b52e06828f47c6c0c08
Nikki <nikkirosecarlos@gmail.com>
2024-06-05 19:41:58 +0900

    call __str__ instead in ClientAdmin

--- clients/admin.py
+++ clients/admin.py
@@ -3,13 +3,8 @@ from .models import Client
 
 class ClientAdmin(admin.ModelAdmin):
     list_display = [
-        'full_name',
+        '__str__',
         'contact_method',
     ]
-
-    def full_name(self, obj):
-        return f"{obj.first_name} {obj.last_name}"
-
-    full_name.short_description = 'Client Name'
 
 admin.site.register(Client, ClientAdmin)
